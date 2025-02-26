172b9e35244fa4baca451a37c7f3aba5af75b6be
Kelly Popko <kelpopko@gmail.com>
2025-02-26 14:59:24 -0600

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+      <footer>
+        <div>
+          logo
+        </div>
+        <nav>
+          <ul>
+            <li>Hamburger menu</li>
+            <li>Home</li>
+            <li>Portfolio</li>
+            <li>Contact Me</li>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+          </ul>
+        </nav>
+      </footer>
+  )
+}

0275dff20f4410cdd02f9728b43c12f812905c94
Kelly Popko <kelpopko@gmail.com>
2025-02-26 14:51:23 -0600

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <h1>Hello Kells</h1>
+    <>
+      <Header />
+    </>
   )
 }
 

97f679335f2a7b71e19e83c1fbb72d6d6b7b8417
Kelly Popko <kelpopko@gmail.com>
2025-02-26 16:20:17 -0600

    Add CSS to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
 export default function Header() {
   return (
-    <header>
-        <div>
-          logo
-        </div>
-        <nav>
-          <ul className="font-public-sans">
-            <li>Hamburger menu</li>
-            <li>Home</li>
-            <li>Portfolio</li>
-            <li>Contact Me</li>
-          </ul>
-        </nav>
-      </header>
+    <header style={{
+      color: 'var(--greyish-dark-blue)',
+      backgroundColor: 'var(--very-light-grey)',
+      textTransform: 'uppercase'
+    }}>
+      <div>
+        logo
+      </div>
+      <nav>
+        <ul className="font-public-sans">
+          <li>Hamburger menu</li>
+          <li>Home</li>
+          <li>Portfolio</li>
+          <li>Contact Me</li>
+        </ul>
+      </nav>
+    </header>
   )
 }

c1316ec1cc0b2e827642b97453a24b21e948b993
Kelly Popko <kelpopko@gmail.com>
2025-02-26 14:51:23 -0600

    Add Header
    Signed-off-by: Kelly Popko <kelpopko@gmail.com>

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import { HamburgerMenu } from '../../assets/icons/HamburgerMenu'
+import { Logo } from '../../assets/icons/Logo'
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-4 md:px-14 md:py-8 lg:py-4" >
+      <div>
+        <Logo />
+      </div>
+      <nav>
+        <ul className="uppercase hidden md:flex md:gap-2">
+          <li>Home</li>
+          <li>Portfolio</li>
+          <li>Contact Me</li>
+        </ul>
+      </nav>
+      <div className="block md:hidden">
+        <HamburgerMenu />
+      </div>
+    </header>
+  )
+}

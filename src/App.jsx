e9432cb33e5a59e9cd159f07af7281619b7b56a9
Kelly Popko <kelpopko@gmail.com>
2025-03-04 20:50:02 -0600

    Add LandingPage component
    - with placeholder hero image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
+import { LandingPage } from './pages/LandingPage'
 
 function App() {
 
   return (
     <>
+      <Header />
+      <LandingPage />
       <Footer />
     </>
   )

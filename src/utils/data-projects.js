351f12e75b06c9edf90274477ef9b5e14b172f51
Kelly Popko <kelpopko@gmail.com>
2025-03-31 14:15:15 -0500

    Add projects to data-projects.js
    - Starting with temperature converter (which itself is wip)
    - Mastermind and Hangman games (which need improving)
    
    To Dos on the work itself:
    - improve either/both games
    - make a TODO checklist - e.g. Rails
    - INCI list maker (for a formulation)
    
    Signed-off-by: Kelly Popko <kelpopko@gmail.com>

---
+++ src/utils/data-projects.js
@@ -0,0 +1,21 @@
+export const PROJECTS = [
+  // Array of each project I choose to highlight
+  {
+    id: 1,
+    name: "Temperature Converter",
+    description: "",
+    image: "/images/projects/temperature-converter.webp",
+  },
+  {
+    id: 2,
+    name: "Mastermind",
+    description: "Text-based Ruby implementation of the classic Mastermind game. Player attempts to crack a 4-color code in 12 attempts. Command line intreface.",
+    image: "/images/projects/to-do-list.webp",
+  },
+  {
+    id: 3,
+    name: "Hangman",
+    description: "A text-based version of the classic game, Hangman to play in your terminal.",
+    image: "/images/projects/hangman.webp",
+  }
+]

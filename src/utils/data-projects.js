64d491d41443dbec55d45ad38ef586412deede94
Kelly Popko <kelpopko@gmail.com>
2025-04-03 14:36:00 -0500

    Update file path in projects

--- src/utils/data-projects.js
+++ src/utils/data-projects.js
@@ -3,24 +3,24 @@ export const PROJECTS = [
     id: 1,
     name: "Temperature Converter",
     description: "Converting temperatures since 2025.",
-    image: "public/assets/images/projects/temperature-converter.png",
+    image: "/assets/images/projects/temperature-converter.png",
   },
   {
     id: 2,
     name: "Mastermind",
     description: "Text-based Ruby implementation of the classic Mastermind game. Player attempts to crack a 4-color code in 12 attempts. Command line intreface.",
-    image: "public/assets/images/projects/mastermind.png",
+    image: "/assets/images/projects/mastermind.png",
   },
   {
     id: 3,
     name: "Hangman",
     description: "A text-based version of the classic game, Hangman to play in your terminal.",
-    image: "public/assets/images/projects/hangman.png",
+    image: "/assets/images/projects/hangman.png",
   },
   {
     id: 4,
     name: "INCI List Maker",
     description: "Create list of INCI ingredients from spreadsheet recipe.",
-    image: "public/assets/images/projects/inci-list-maker.png",
+    image: "/assets/images/projects/inci-list-maker.png",
   }
 ]

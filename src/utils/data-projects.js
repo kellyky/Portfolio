68fcb9b9412c9d4a0a641000a19f5d4aada749dc
Kelly Popko <kelpopko@gmail.com>
2025-04-08 14:41:17 -0500

    [wip][remove] Use "stock" project descriptions
    - temporary

--- src/utils/data-projects.js
+++ src/utils/data-projects.js
@@ -2,25 +2,29 @@ export const PROJECTS = [
   {
     id: 1,
     name: 'Temperature Converter',
-    description: 'Converting temperatures since 2025.',
-    image: '/assets/images/projects/temperature-converter.png'
+    description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
+    image: '/assets/images/projects/temperature-converter.png',
+    imageDesktop: '/assets/images/projects/temperature-converter-desktop.png'
   },
   {
     id: 2,
     name: 'Mastermind',
-    description: 'Text-based Ruby implementation of the classic Mastermind game. Player attempts to crack a 4-color code in 12 attempts. Command line intreface.',
-    image: '/assets/images/projects/mastermind.png'
+    description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
+    image: '/assets/images/projects/mastermind.png',
+    imageDesktop: '/assets/images/projects/mastermind-desktop.png'
   },
   {
     id: 3,
     name: 'Hangman',
-    description: 'A text-based version of the classic game, Hangman to play in your terminal.',
-    image: '/assets/images/projects/hangman.png'
+    description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
+    image: '/assets/images/projects/hangman.png',
+    imageDesktop: '/assets/images/projects/hangman-desktop.png'
   },
   {
     id: 4,
     name: 'INCI List Maker',
-    description: 'Create list of INCI ingredients from spreadsheet recipe.',
-    image: '/assets/images/projects/inci-list-maker.png'
+    description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
+    image: '/assets/images/projects/inci-list-maker.png',
+    imageDesktop: '/assets/images/projects/inci-list-maker-desktop.png'
   }
 ]

18cf518fd04444f242f1b8c948ba42cbfc0e0772
Kelly (Young) Popko <kelpopko@gmail.com>
2025-04-28 15:58:31 -0500

    Update PROJECTS:
    - Add `project` key to each project
    
    For each `project`:
    - Add image URLs: hero and static previews | mobile, tablet, desktop
    - Add languages
    - Add concepts (e.g. Backend development)

--- src/utils/data-projects.js
+++ src/utils/data-projects.js
@@ -4,27 +4,104 @@ export const PROJECTS = [
     name: 'Temperature Converter',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
     image: '/assets/images/projects/portfolioPage/temperature-converter.png',
-    imageDesktop: '/assets/images/projects/portfolioPage/temperature-converter-desktop.png'
+    imageDesktop: '/assets/images/projects/portfolioPage/temperature-converter-desktop.png',
+    projectInfo: {
+      languages: 'Ruby / RI / Ruby Doctest / Back End Development',
+      concepts: 'Back End Development / Documentation / Command Line Tooling / Ruby Library',
+      hero: [
+        '/assets/images/projects/projectPages/projectZero/hero-mobile.png',
+        '/assets/images/projects/projectPages/projectZero/hero-tablet.png',
+        '/assets/images/projects/projectPages/projectZero/hero-desktop.png',
+      ],
+      previewOne: [
+        '/assets/images/projects/projectPages/projectZero/staticOne-mobile.png',
+        '/assets/images/projects/projectPages/projectZero/staticOne-tablet.png',
+        '/assets/images/projects/projectPages/projectZero/staticOne-desktop.png',
+      ],
+      previewTwo: [
+        '/assets/images/projects/projectPages/projectZero/staticTwo-mobile.png',
+        '/assets/images/projects/projectPages/projectZero/staticTwo-tablet.png',
+        '/assets/images/projects/projectPages/projectZero/staticTwo-desktop.png'
+      ]
+    }
+
   },
   {
     id: 2,
     name: 'Mastermind',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
     image: '/assets/images/projects/portfolioPage/mastermind.png',
-    imageDesktop: '/assets/images/projects/portfolioPage/mastermind-desktop.png'
+    imageDesktop: '/assets/images/projects/portfolioPage/mastermind-desktop.png',
+    projectInfo: {
+      languages: 'Ruby / RSpec',
+      concepts: 'Back End Development / Command Line',
+      hero: [
+        '/assets/images/projects/projectPages/projectOne/hero-mobile.png',
+        '/assets/images/projects/projectPages/projectOne/hero-tablet.png',
+        '/assets/images/projects/projectPages/projectOne/hero-desktop.png',
+      ],
+      previewOne: [
+        '/assets/images/projects/projectPages/projectOne/staticOne-mobile.png',
+        '/assets/images/projects/projectPages/projectOne/staticOne-tablet.png',
+        '/assets/images/projects/projectPages/projectOne/staticOne-desktop.png',
+      ],
+      previewTwo: [
+        '/assets/images/projects/projectPages/projectOne/staticTwo-mobile.png',
+        '/assets/images/projects/projectPages/projectOne/staticTwo-tablet.png',
+        '/assets/images/projects/projectPages/projectOne/staticTwo-desktop.png'
+      ]
+    }
   },
   {
     id: 3,
     name: 'Hangman',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
     image: '/assets/images/projects/portfolioPage/hangman.png',
-    imageDesktop: '/assets/images/projects/portfolioPage/hangman-desktop.png'
+    imageDesktop: '/assets/images/projects/portfolioPage/hangman-desktop.png',
+    projectInfo: {
+      languages: 'Ruby / IO',
+      concepts: 'Back End Development / Command Line Game',
+      hero: [
+        '/assets/images/projects/projectPages/projectTwo/hero-mobile.png',
+        '/assets/images/projects/projectPages/projectTwo/hero-tablet.png',
+        '/assets/images/projects/projectPages/projectTwo/hero-desktop.png',
+      ],
+      previewOne: [
+        '/assets/images/projects/projectPages/projectTwo/staticOne-mobile.png',
+        '/assets/images/projects/projectPages/projectTwo/staticOne-tablet.png',
+        '/assets/images/projects/projectPages/projectTwo/staticOne-desktop.png',
+      ],
+      previewTwo: [
+        '/assets/images/projects/projectPages/projectTwo/staticTwo-mobile.png',
+        '/assets/images/projects/projectPages/projectTwo/staticTwo-tablet.png',
+        '/assets/images/projects/projectPages/projectTwo/staticTwo-desktop.png'
+      ]
+    }
   },
   {
     id: 4,
     name: 'INCI List Maker',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
     image: '/assets/images/projects/portfolioPage/inci-list-maker.png',
-    imageDesktop: '/assets/images/projects/portfolioPage/inci-list-maker-desktop.png'
+    imageDesktop: '/assets/images/projects/portfolioPage/inci-list-maker-desktop.png',
+    projectInfo: {
+      languages: 'Ruby',
+      concepts: 'Back End Development',
+      hero: [
+        '/assets/images/projects/projectPages/projectThree/hero-mobile.png',
+        '/assets/images/projects/projectPages/projectThree/hero-tablet.png',
+        '/assets/images/projects/projectPages/projectThree/hero-desktop.png',
+      ],
+      previewOne: [
+        '/assets/images/projects/projectPages/projectThree/staticOne-mobile.png',
+        '/assets/images/projects/projectPages/projectThree/staticOne-tablet.png',
+        '/assets/images/projects/projectPages/projectThree/staticOne-desktop.png',
+      ],
+      previewTwo: [
+        '/assets/images/projects/projectPages/projectThree/staticTwo-mobile.png',
+        '/assets/images/projects/projectPages/projectThree/staticTwo-tablet.png',
+        '/assets/images/projects/projectPages/projectThree/staticTwo-desktop.png'
+      ]
+    }
   }
 ]

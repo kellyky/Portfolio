0a695eff7d567d942e1c7665a4d711f38b4dd0d5
Kelly (Young) Popko <kelpopko@gmail.com>
2025-04-28 15:30:27 -0500

    Organize photos:
    - create directory for portfolioPage photos

--- src/utils/data-projects.js
+++ src/utils/data-projects.js
@@ -3,28 +3,28 @@ export const PROJECTS = [
     id: 1,
     name: 'Temperature Converter',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
-    image: '/assets/images/projects/temperature-converter.png',
-    imageDesktop: '/assets/images/projects/temperature-converter-desktop.png'
+    image: '/assets/images/projects/portfolioPage/temperature-converter.png',
+    imageDesktop: '/assets/images/projects/portfolioPage/temperature-converter-desktop.png'
   },
   {
     id: 2,
     name: 'Mastermind',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
-    image: '/assets/images/projects/mastermind.png',
-    imageDesktop: '/assets/images/projects/mastermind-desktop.png'
+    image: '/assets/images/projects/portfolioPage/mastermind.png',
+    imageDesktop: '/assets/images/projects/portfolioPage/mastermind-desktop.png'
   },
   {
     id: 3,
     name: 'Hangman',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
-    image: '/assets/images/projects/hangman.png',
-    imageDesktop: '/assets/images/projects/hangman-desktop.png'
+    image: '/assets/images/projects/portfolioPage/hangman.png',
+    imageDesktop: '/assets/images/projects/portfolioPage/hangman-desktop.png'
   },
   {
     id: 4,
     name: 'INCI List Maker',
     description: 'This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.',
-    image: '/assets/images/projects/inci-list-maker.png',
-    imageDesktop: '/assets/images/projects/inci-list-maker-desktop.png'
+    image: '/assets/images/projects/portfolioPage/inci-list-maker.png',
+    imageDesktop: '/assets/images/projects/portfolioPage/inci-list-maker-desktop.png'
   }
 ]

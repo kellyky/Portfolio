1004c1caa37798275bc646ec7227e8bf5a703ccc
Kelly (Young) Popko <kelpopko@gmail.com>
2025-04-30 15:27:24 -0500

    Style pagination navigation buttons
    i.e. where to view previous/next project at the bottom of Project page

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -11,26 +11,46 @@ export default function Pagination () {
     handleNextPage
   ] = usePagination()
 
+  // styles
+  const headingStyle = `font-ibarra tracking-light text-dark-greyish-blue
+                        max-[420px]:text-2xl max-[500px]:text-3xl text-4xl`
+  const buttonStyle  = `flex flex-col justify-between gap-2 lg:gap-4 py-6`
+  const textStyle    = `font-public-sans opacity-50 text-dark-greyish-blue text-base`
+
   return (
     <>
-      <section>
+      <section
+        className='
+        grid grid-cols-2 divide-x-2 divide-solid divide-greyish-dark-blue/10
+        border-y-2 border-greyish-dark-blue/10
+        '
+      >
         <Button
-          isButton={true}
+          isButton ={true}
           onClick={handlePreviousPage}
-          className=''
+          className={`${buttonStyle} items-start`}
         >
           <PageLeft />
-          {previousProject.name}
-          Previous Project
+          <h2 className={`${headingStyle} text-left`}>
+            {previousProject.name}
+          </h2>
+
+          <span className={`${textStyle} text-left`}>
+            Previous Project
+          </span>
         </Button>
         <Button
           isButton={true}
           onClick={handleNextPage}
-          className=''
+          className={`${buttonStyle} items-end`}
         >
           <PageRight />
-          {nextProject.name}
-          Next Project
+          <h2 className={ `${headingStyle} text-right`} >
+              {nextProject.name}
+          </h2>
+          <span className={`${textStyle} text-right`}>
+            Next Project
+          </span>
         </Button>
       </section>
     </>

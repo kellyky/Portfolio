1fc9f5cfe8ed3b078e3b120b6239c9ac419dd79a
Kelly (Young) Popko <kelpopko@gmail.com>
2025-05-08 16:52:00 -0500

    [wip] Update NotfoundPage
    - add background image
    - add button linking to home

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,24 @@
+import Button from '../components/Button/Button'
+
 function NotFoundPage () {
   return (
     <>
-      <h1>Ope, nothing here</h1>
+      <div
+        className='
+        h-screen bg-not-found-page
+        flex flex-col items-center justify-center
+        '
+      >
+        <h1
+          className=''
+        >Error 404: Page Not Found</h1>
+        <Button
+          isButton={false}
+          to='/'
+        >
+          Go Home
+        </Button>
+      </div>
     </>
   )
 }

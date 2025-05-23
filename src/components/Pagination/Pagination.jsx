f6d4c448e0fbf3f2e7448e24237925f860f2fb08
Kelly (Young) Popko <kelpopko@gmail.com>
2025-05-23 15:32:58 -0500

    Style Project Pagination for Desktop view

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -14,7 +14,8 @@ export default function Pagination () {
   // styles
   const headingStyle = `text-dark-greyish-blue
                         max-[420px]:text-2xl max-[500px]:text-3xl text-4xl`
-  const buttonStyle  = `flex flex-col justify-between gap-2 py-4`
+  const buttonStyle  = `flex flex-col justify-between gap-2 py-4
+                        lg:flex-row lg:items-center lg:gap-4`
   const textStyle    = `opacity-50 text-dark-greyish-blue`
 
   return (
@@ -28,29 +29,34 @@ export default function Pagination () {
         <Button
           isButton ={true}
           onClick={handlePreviousPage}
-          className={`${buttonStyle} items-start`}
+          className={`${buttonStyle} items-start lg:justify-start`}
         >
           <PageLeft />
-          <h2 className={`${headingStyle} text-left`}>
-            {previousProject.name}
-          </h2>
+          <div className='flex flex-col'>
+            <h2 className={`${headingStyle} text-left`}>
+              {previousProject.name}
+            </h2>
 
-          <span className={`${textStyle} text-left`}>
-            Previous Project
-          </span>
+            <span className={`${textStyle} text-left`}>
+              Previous Project
+            </span>
+          </div>
         </Button>
         <Button
           isButton={true}
           onClick={handleNextPage}
-          className={`${buttonStyle} items-end`}
+          className={`${buttonStyle} items-end lg:justify-start lg:flex-row-reverse
+          `}
         >
           <PageRight />
-          <h2 className={ `${headingStyle} text-right`} >
-              {nextProject.name}
-          </h2>
-          <span className={`${textStyle} text-right`}>
-            Next Project
-          </span>
+          <div className='flex flex-col'>
+            <h2 className={`${headingStyle} text-right`} >
+                {nextProject.name}
+            </h2>
+            <span className={`${textStyle} text-right`}>
+              Next Project
+            </span>
+          </div>
         </Button>
       </section>
     </>

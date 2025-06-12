64aed550fdce9bb6c139ade75232fea450bd74df
Kelly (Young) Popko <kelpopko@gmail.com>
2025-06-12 10:31:35 -0500

    Style FormSuccessModal for tablet

--- src/components/FormSuccessModal/FormSuccessModal.jsx
+++ src/components/FormSuccessModal/FormSuccessModal.jsx
@@ -18,66 +18,87 @@ export default function FormSuccessModal () {
           numberOfPieces={150}
         /> }
 
-      <dialog
-        className='
-        flex
-        flex-col
-        rounded-lg
-        w-2/3
-        inset-0
-        fixed
-        max-w-dvw
-        h-max-dvh
-        m-auto
-        h-[5rem]
-        '
-        open
-      >
-        <img
-          src='/assets/images/contact-confirmation/beach_umbrella.webp'
-          alt='Black and white sketch of a beach and umbrella'
-          className='
-          rounded-tl-lg
-          rounded-tr-lg
-          '
-        />
+      <div className='
+        md:size-1/5
+        '>
 
-        <div className='bg-white
-          rounded-bl-lg
-          rounded-br-lg
+        <dialog
+          className='
           flex
           flex-col
-          md:flex-row
-          justify-between
-          items-center
-          p-4
-          '>
-          <div
-            className='
-            flex flex-col items-center gap-2
-            '
-          >
-            <strong>Thanks for reaching out!</strong>
-            <div
-              className='flex items-center gap-2'
-            >
-              <p>I will be in touch soon.</p>
-              <PaperAirplane
-                className='
-                fill-greyish-dark-blue
-                size-6
-                '
-              />
-            </div>
-          </div>
-          <Button
-            className=''
-          >
-            back
-          </Button>
-        </div>
+          rounded-lg
+          w-2/3
+          inset-0
+          fixed
+          max-w-dvw
+          h-max-dvh
+          m-auto
+          h-[5rem]
 
-      </dialog>
+          md:flex-row
+          md:h-fit
+          md:pb-4
+          '
+          open
+        >
+          <img
+            src='/assets/images/contact-confirmation/beach_umbrella.webp'
+            alt='Black and white sketch of a beach and umbrella'
+            className='
+            rounded-tl-lg
+            rounded-tr-lg
+
+            md:w-1/2
+            md:h-fit
+            '
+          />
+
+          <div className='bg-white
+            rounded-bl-lg
+            rounded-br-lg
+            flex
+            flex-col
+            justify-between
+            items-center
+            p-4
+
+            md:flex-col
+            md:gap-[1rem]
+
+            '>
+            <div
+              className='
+              flex flex-col items-center
+              
+              '
+            >
+              <h3
+                className='
+                font-bold
+                text-xl
+                '
+              >Thanks for reaching out!</h3>
+              <div
+                className='flex items-center gap-2'
+              >
+                <p>I will be in touch soon.</p>
+                <PaperAirplane
+                  className='
+                  fill-greyish-dark-blue
+                  size-6
+                  '
+                />
+              </div>
+            </div>
+            <Button
+              className=''
+            >
+              back
+            </Button>
+          </div>
+
+        </dialog>
+      </div>
 
     </>,
     document.body

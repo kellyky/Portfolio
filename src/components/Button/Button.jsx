80df360af528cddaa727f818422e56557dc676ce
Kelly Popko <kelpopko@gmail.com>
2025-07-14 11:23:39 -0500

    Add style for button hover
    - Specifically the white background/dark blue outline buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,6 +6,8 @@ export default function Button ({ children, isButton, ...args }) {
         className='w-52 p-4 min-w-52 h-min flex justify-center
         border border-greyish-dark-blue
         uppercase text-xs tracking-widest
+        hover:bg-greyish-dark-blue
+        hover:text-very-light-grey
         '>
           {children}
       </Link>

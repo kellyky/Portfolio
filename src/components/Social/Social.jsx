361ef79d763462dd10923114ba74430a36957791
Kelly Popko <kelpopko@gmail.com>
2025-07-14 09:45:58 -0500

    Add Social Media Icons to Socials
    - Dev
    - Medium
    - Bluesky

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -1,5 +1,8 @@
+import { BlueSky } from '../../assets/icons/BlueSky'
+import { Dev } from '../../assets/icons/Dev'
 import { GitHub } from '../../assets/icons/GitHub'
 import { LinkedIn } from '../../assets/icons/LinkedIn'
+import { Medium } from '../../assets/icons/Medium'
 
 export default function Social ({color}) {
   return (
@@ -23,6 +26,42 @@ export default function Social ({color}) {
             aria-label="Kelly's LinkedIn profile" rel='noreferrer'
           >
             <LinkedIn
+              className={`hover:fill-muted-cyan transition-colors ${color}
+              delay-150 duration-300`}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://dev.to/kelpopko'
+            target='_blank'
+            aria-label="Kelly's Dev.to profile" rel='noreferrer'
+          >
+            <Dev
+              className={`hover:fill-muted-cyan transition-colors ${color}
+              delay-150 duration-300`}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://medium.com/@kelpopko'
+            target='_blank'
+            aria-label="Kelly's Medium profile" rel='noreferrer'
+          >
+            <Medium
+              className={`hover:fill-muted-cyan transition-colors ${color}
+              delay-150 duration-300`}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://bsky.app/profile/kellypopko.bsky.social'
+            target='_blank'
+            aria-label="Kelly's BlueSky profile" rel='noreferrer'
+          >
+            <BlueSky
               className={`hover:fill-muted-cyan transition-colors ${color}
               delay-150 duration-300`}
             />

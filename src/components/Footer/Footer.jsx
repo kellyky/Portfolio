0f64e4d784eaa1b181a368514e8944d676165f3b
Kelly Popko <kelpopko@gmail.com>
2025-08-26 15:54:11 -0500

    Add Resume link to Footer
    Update Footer style to accommodate Resume

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,26 @@
 import { Link } from 'react-router'
 import { Logo } from '../../assets/icons/Logo'
 import Social from '../Social/Social'
+import Resume from '/resume/Resume.pdf'
 
 export default function Footer () {
   return (
-    <footer className='bottom-0 p-12 md:p-4 gap-8 lg:px-20
+    <footer className='bottom-0 md:p-4 gap-8 lg:px-20
       flex flex-col md:flex-row items-center justify-between
       bg-greyish-dark-blue text-very-light-grey'
     >
-      <nav>
-        <ul className='uppercase px-10 gap-8 flex flex-col
+      <nav className='w-full m-8 gap-8 md:gap-4 flex flex-col md:flex-row md:justify-between items-center'>
+        <ul className='uppercase gap-6 flex flex-col
           md:flex-row items-center'
         >
           <li><Logo fill='var(--very-light-grey)' /></li>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/portfolio'>Portfolio</Link></li>
+          <li><Link to={Resume} target='_blank'>Resume</Link></li>
           <li><Link to='/contact'>Contact Me</Link></li>
         </ul>
-      </nav>
       <Social color='fill-very-light-grey' />
+      </nav>
     </footer>
   )
 }
